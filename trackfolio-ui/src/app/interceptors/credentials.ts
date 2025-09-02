ca10c9c6b918f5d793dde18683ea2143332d7011
cschere1 <cschere1@gmail.com>
2025-09-02 16:01:15 -0500

    Prep: remove local copies before subtree import

--- trackfolio-ui/src/app/interceptors/credentials.ts
+++
@@ -1,7 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const credentialsInterceptor: HttpInterceptorFn = (req, next) => {
-  // Ensure cookies (session) are sent to the Spring Boot backend via the dev proxy
-  const withCreds = req.clone({ withCredentials: true });
-  return next(withCreds);
-};
